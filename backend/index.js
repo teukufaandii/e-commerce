8e554632a1d7bfa52ec7483a0128dea42bb42d6f
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2024-10-19 14:14:50 +0700

    sql connection and frontend setup

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import mysqlDbConnect from "./db/mysqlConnect.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+const PORT = process.env.PORT || 8000;
+
+app.use("/api/test", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  mysqlDbConnect;
+});
